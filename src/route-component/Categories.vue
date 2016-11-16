17e78f612273ff8da42d51ae70cdb9b47d918b8d
Joker <591713854@qq.com>
2016-11-16 15:13:02 +0800

    review mounted function

--- src/route-component/Categories.vue
+++ src/route-component/Categories.vue
@@ -1,23 +1,21 @@
 <template>
     <div id="category">
         <category-item :category="category" :asTitle="true"></category-item>
+        <!--<albums-list :albums="albums"></albums-list>-->
     </div>
+  </div>
 
 </template>
 
 <script>
     import CategoryItem from '../component/Category.vue'
-    
+    import Category from '../model/Categories.js'
     export default {
         name: 'CategoryPage',
         data(){
             return {
                 // 分类信息
-                category: {
-                    title: '',
-                    cover: '',
-                    subtitle: 'subtitle'
-                },
+                category: {},
                 // 相册信息
                 albums: []
             }
@@ -28,7 +26,15 @@
         mounted() {
             this.$nextTick(function () {
             // 保证 this.$el 已经插入文档
-                this.category.title = this.$route.params.category
+                // this.category.title = this.$route.params.category
+                Promise.all([
+                    Category.loadIfNotInit()
+                    .then( () => Category.search('name' ,this.$route.params.category))
+                    .then(result => result[0])
+                ])
+                .then( ([category]) => {
+                    this.category = category.getCacheData()
+                })
             })
         }
     }
