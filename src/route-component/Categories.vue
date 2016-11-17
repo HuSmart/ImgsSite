7e3606ff5c369e0c254c0c2770f5ee59a14feb0b
Joker <591713854@qq.com>
2016-11-17 12:37:22 +0800

    albums content

--- src/route-component/Categories.vue
+++ src/route-component/Categories.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="category">
         <category-item :category="category" :asTitle="true"></category-item>
-        <!--<albums-list :albums="albums"></albums-list>-->
+        <albums-list :albums="albums"></albums-list>
     </div>
   </div>
 
@@ -10,6 +10,9 @@
 <script>
     import CategoryItem from '../component/Category.vue'
     import Category from '../model/Categories.js'
+    import Album from '../model/Album.js'
+    import AlbumsList from '../component/AlbumList.vue'
+
     export default {
         name: 'CategoryPage',
         data(){
@@ -21,7 +24,8 @@
             }
         },
         components: {
-            CategoryItem
+            CategoryItem,
+            AlbumsList
         },
         mounted() {
             this.$nextTick(function () {
@@ -29,11 +33,13 @@
                 // this.category.title = this.$route.params.category
                 Promise.all([
                     Category.loadIfNotInit()
-                    .then( () => Category.search('name' ,this.$route.params.category))
-                    .then(result => result[0])
+                    .then( () => Category.search('name' ,this.$route.params.name))
+                    .then(result => result[0]),
+                    Album.fetchByCategory(this.$route.params.name)
                 ])
-                .then( ([category]) => {
+                .then( ([category, album]) => {
                     this.category = category.getCacheData()
+                    this.albums = album.map(n => n.getCacheData())
                 })
             })
         }
