f8095a9e7d981734021117dd98756d9efff7e6ba
Joker <591713854@qq.com>
2016-11-22 16:44:42 +0800

    相册类别管理视图组件

---
+++ src/route-component/admin/admin-categories.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="categories">
+        <div class="page-header"></div>
+        <div class="row">
+            <form action="" class="form-inline pull-right">
+                <div class="form-group">
+                    <button class="btn btn-primary">new</button>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control">
+                    <button class="btn">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'admin-category',
+        data(){
+            return {}
+        }
+    }
+</script>
+<style>
+
+    .row{
+        margin-right: 0px
+    }
+</style>
